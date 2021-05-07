a8100bfab69000c51625fd8354e91166a079966d
GitHub Action <action@github.com>
2021-05-07 13:06:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/moonchaserchen/hwideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/moonchaserchen/hwideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.moonchaserchen.hwideaplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/moonchaserchen/hwideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.moonchaserchen.hwideaplugin.services
+
+import com.github.moonchaserchen.hwideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
